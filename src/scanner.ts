00c3c5004e0f90a07c16550550b2e0230b790d00
SyMind <dacongsama@live.com>
2022-11-09 22:05:03 +0800

    dev scanner

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,28 +1,5 @@
-export enum TokenType {
-	Unknown,
-
-	Prefix,
-	Numeric,
-	Boolean,
-	String,
-	Pattern,
-	Time,
-	Variable,
-	Function,
-	Separator,
-	Accessor,
-
-	Comparator,
-	Logicalop,
-	Modifier,
-
-	Clause,
-	Clause_close,
-
-	Ternary,
-
-    EOF
-}
+import { Diagnostic, Position, Range } from 'vscode-languageserver-types'
+import { CharacterCodes, SyntaxKind } from './typings'
 
 export class MultiLineStream {
     private source: string
@@ -102,28 +79,6 @@ export class MultiLineStream {
 	}
 }
 
-enum CharacterCodes {
-    // Unicode's White Space
-    tab = 0x09,                   // \t
-    lineFeed = 0x0A,              // \n
-    verticalTab = 0x0B,           // \v
-    formFeed = 0x0C,              // \f
-    carriageReturn = 0x0D,        // \r
-    space = 0x0020,               // " "
-
-    a = 0x61,
-    f = 0x66,
-    x = 0x78,
-
-    _0 = 0x30,
-    _9 = 0x39,
-
-    comma = 0x2C,                 // ,
-    dot = 0x2E,                   // .
-    openBracket = 0x5B,           // [
-    closeBracket = 0x5D,          // ]
-}
-
 function isDigit(ch: number): boolean {
     return ch >= CharacterCodes._0 && ch <= CharacterCodes._9
 }
@@ -132,22 +87,57 @@ function isHexDigit(ch: number): boolean {
     return isDigit(ch) || ch >= CharacterCodes.a && ch <= CharacterCodes.f
 }
 
+function isLetter(ch: number): boolean {
+    return (65 <= ch && ch <= 90) || (97 <= ch && ch <= 122) || (ch >= 128 && ch !== 133 && ch !== 160)
+}
+
+function isVariableName(ch: number): boolean {
+    return isLetter(ch) || isDigit(ch) || ch === CharacterCodes._ || ch === CharacterCodes.dot
+}
+
+function isQuote(ch: number): boolean {
+	return ch === CharacterCodes.doubleQuote || ch === CharacterCodes.singleQuote
+}
+
 export interface IToken {
-	type: TokenType
+	type: SyntaxKind
 	text: string
 	offset: number
 	len: number
 }
 
+type ErrorCallback = (message: Diagnostic) => void
+
 export class Scanner {
     stream = new MultiLineStream('')
-	ignoreComment = true
-	ignoreWhitespace = true
-	inURL = false
+
+    functions: string[]
+
+    constructor(functions: string[]) {
+        this.functions = functions
+    }
+
+    onError?: ErrorCallback
 
     setSource(input: string): void {
 		this.stream = new MultiLineStream(input)
 	}
+
+    setOnError(errorCallback: ErrorCallback | undefined) {
+        this.onError = errorCallback
+    }
+
+    error(message: string, start: number, length?: number) {
+        if (this.onError) {
+            const s = start ?? this.stream.pos()
+            const l = length ?? 0
+            const range = Range.create(Position.create(0, s), Position.create(s, s + l))
+            this.onError({
+                range,
+                message
+            })
+        }
+    }
 
     scan(): IToken {
 		// processes all whitespaces
@@ -157,31 +147,237 @@ export class Scanner {
 
 		// End of file/input
 		if (this.stream.eos()) {
-			return this.finishToken(offset, TokenType.EOF)
+			return this.finishToken(offset, SyntaxKind.EndOfFileToken)
 		}
 		return this.scanNext(offset)
 	}
 
     scanNext(offset: number): IToken {
+        const ch = this.stream.peekChar()
+
 		// numeric constant
-		if (this._numeric()) {
-			return this.finishToken(offset, TokenType.Numeric)
-		}
+        if (ch === CharacterCodes._0 && this.stream.peekChar(1) === CharacterCodes.x) {
+            this.stream.advance(2)
+            let expected = false
+            this.stream.advanceWhileChar(ch => {
+                if (isHexDigit(ch)) {
+                    expected = true
+                    return true
+                }
+                return false
+            })
+            if (!expected) {
+                this.error('Hexadecimal digit expected.', offset)
+            }
+            return this.finishToken(offset, SyntaxKind.NumericLiteral)
+        }
+        if (ch >= CharacterCodes._0 && ch <= CharacterCodes._9 || ch === CharacterCodes.dot) {
+            let expected = false
+            this.stream.advanceWhileChar(ch => {
+                if (isDigit(ch)) {
+                    expected = true
+                    return true
+                }
+                return false
+            })
+            if (!expected) {
+                this.error('Numeric literal expected.', offset)
+            }
+            return this.finishToken(offset, SyntaxKind.NumericLiteral)
+        }
 
         // comma, separator
-        if (this.stream.peekChar() === CharacterCodes.comma) {
-            return this.finishToken(offset, TokenType.Separator)
+        if (ch === CharacterCodes.comma) {
+            return this.finishToken(offset, SyntaxKind.CommaToken)
         }
 
         // escaped variable
-        // if (this.stream.peekChar() === CharacterCodes.openBracket) {
-            
-		// }
+        if (ch === CharacterCodes.openBracket) {
+            this.stream.advance(1)
+            let completed = false
+            this.stream.advanceWhileChar(ch => {
+                if (ch === CharacterCodes.closeBracket) {
+                    completed = true
+                    return false
+                }
+                return true
+            })
+            if (!completed) {
+                this.error('Unclosed parameter bracket', offset)
+            }
+            return this.finishToken(offset, SyntaxKind.StringLiteral)
+		}
+
+        // variable - or function
+        if (isLetter(ch)) {
+            this.stream.advanceWhileChar(isVariableName)
+            const tokenString = this.stream.substring(offset)
+            if (tokenString === 'true' || tokenString === 'false') {
+                return this.finishToken(offset, SyntaxKind.BooleanKeyword)
+            }
+            if (tokenString === 'in' || tokenString === 'IN') {
+                return this.finishToken(offset, SyntaxKind.CommaToken)
+            }
+            // function?
+            let kind: SyntaxKind
+            if (this.functions.includes[tokenString]) {
+                kind = SyntaxKind.Function
+            }
+            // accessor?
+            const accessorIndex = tokenString.indexOf('.')
+            if (accessorIndex > 0) {
+                // check that it doesn't end with a hanging period
+				if (tokenString[tokenString.length - 1] === '.') {
+                    this.error(`Hanging accessor on token ${tokenString}`, offset)
+				}
+                kind = SyntaxKind.Accessor
+                const splits = tokenString.split('.')
+				
+                // check that none of them are unexported
+				for (let i = 1; i < splits.length; i++) {
+					const firstCharacter = splits[i]
+                    if (firstCharacter.toUpperCase() !== firstCharacter) {
+                        this.error(`Unable to access unexported field '${splits[i]}' in token '${tokenString}'`, offset)
+                    }
+				}
+                return this.finishToken(offset, SyntaxKind.Accessor)
+            }
+        }
+
+        if (isQuote(ch)) {
+            this.stream.advance(1)
+            let completed = false
+            this.stream.advanceWhileChar(ch => {
+                if (isQuote(ch)) {
+                    completed = true
+                    return false
+                }
+                return true
+            })
+            if (!completed) {
+                this.error('Unclosed string literal', offset)
+			}
+            return this.finishToken(offset, SyntaxKind.StringLiteral)
+        }
+
+        // modifier and logical
+        if (ch === CharacterCodes.openParen) {
+            return this.finishToken(offset, SyntaxKind.OpenParenToken)
+        }
+        if (ch === CharacterCodes.closeBracket) {
+            return this.finishToken(offset, SyntaxKind.CloseParenToken)
+        }
+        if (ch === CharacterCodes.plus) {
+            if (this.stream.peekChar(1) === CharacterCodes.plus) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.PlusPlusToken)
+            }
+            return this.finishToken(offset, SyntaxKind.PlusToken)
+        }
+        if (ch === CharacterCodes.minus) {
+            if (this.stream.peekChar(1) === CharacterCodes.minus) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.MinusMinusToken)
+            }
+            return this.finishToken(offset, SyntaxKind.MinusToken)
+        }
+        if (ch === CharacterCodes.asterisk) {
+            return this.finishToken(offset, SyntaxKind.AsteriskToken)
+        }
+        if (ch === CharacterCodes.slash) {
+            if (this.stream.peekChar(1) === CharacterCodes.asterisk) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.AsteriskAsteriskToken)
+            }
+            return this.finishToken(offset, SyntaxKind.SlashToken)
+        }
+        if (ch === CharacterCodes.percent) {
+            return this.finishToken(offset, SyntaxKind.PercentToken)
+        }
+        if (ch === CharacterCodes.ampersand) {
+            if (this.stream.peekChar(1) === CharacterCodes.ampersand) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.AmpersandAmpersandToken)
+            }
+            return this.finishToken(offset, SyntaxKind.AmpersandToken)
+        }
+        if (ch === CharacterCodes.bar) {
+            if (this.stream.peekChar(1) === CharacterCodes.bar) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.BarBarToken)
+            }
+            return this.finishToken(offset, SyntaxKind.BarToken)
+        }
+        if (ch === CharacterCodes.caret) {
+            return this.finishToken(offset, SyntaxKind.CaretToken)
+        }
+
+        // comparator
+        if (ch === CharacterCodes.equals) {
+            if (this.stream.peekChar(1) === CharacterCodes.equals) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.EqualsEqualsToken)
+            }
+            if (this.stream.peekChar(1) === CharacterCodes.tilde) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.EqualsTildeToken)
+            }
+        }
+        if (ch === CharacterCodes.exclamation) {
+            if (this.stream.peekChar(1) === CharacterCodes.equals) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.ExclamationEqualsToken)
+            }
+            if (this.stream.peekChar(1) === CharacterCodes.tilde) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.ExclamationTildeToken)
+            }
+        }
+        if (ch === CharacterCodes.greaterThan) {
+            if (this.stream.peekChar(1) === CharacterCodes.equals) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.GreaterThanEqualsToken)
+            }
+            if (this.stream.peekChar(1) === CharacterCodes.greaterThan) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.GreaterThanGreaterThanToken)
+            }
+            return this.finishToken(offset, SyntaxKind.GreaterThanToken)
+        }
+        if (ch === CharacterCodes.lessThan) {
+            if (this.stream.peekChar(1) === CharacterCodes.equals) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.LessThanEqualsToken)
+            }
+            if (this.stream.peekChar(1) === CharacterCodes.lessThan) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.LessThanLessThanToken)
+            }
+            return this.finishToken(offset, SyntaxKind.LessThanToken)
+        }
+        if (ch === CharacterCodes.i) {
+            if (this.stream.peekChar(1) === CharacterCodes.n) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.InToken)
+            }
+        }
+
+        // ternary
+        if (ch === CharacterCodes.question) {
+            if (this.stream.peekChar(1) === CharacterCodes.question) {
+                this.stream.advance(1)
+                return this.finishToken(offset, SyntaxKind.QuestionQuestionToken)
+            }
+            return this.finishToken(offset, SyntaxKind.QuestionToken)
+        }
+        if (ch === CharacterCodes.colon) {
+            return this.finishToken(offset, SyntaxKind.ColonToken) 
+        }
     
-        return this.finishToken(offset, TokenType.Unknown)
+        return this.finishToken(offset, SyntaxKind.Unknown)
 	}
 
-    finishToken(offset: number, type: TokenType, text?: string): IToken {
+    finishToken(offset: number, type: SyntaxKind, text?: string): IToken {
 		return {
 			offset: offset,
 			len: this.stream.pos() - offset,
@@ -203,17 +399,4 @@ export class Scanner {
         })
 		return n > 0
 	}
-
-    private _numeric(): boolean {
-		const ch = this.stream.peekChar()
-        if (ch === CharacterCodes._0 && this.stream.peekChar(1) === CharacterCodes.x) {
-            this.stream.advance(2)
-            this.stream.advanceWhileChar(isHexDigit)
-            return true
-        } else if (ch >= CharacterCodes._0 && ch <= CharacterCodes._9 || ch === CharacterCodes.dot) {
-            this.stream.advanceWhileChar(isDigit)
-            return true
-        }
-        return false
-    }
 }
--- src/typings.ts
+++ src/typings.ts
@@ -1,17 +1,73 @@
+import { Diagnostic } from 'vscode-languageserver-types'
+
 export const enum SyntaxKind {
+    Unknown,
+    EndOfFileToken,
+    Goevaluate,
+
     // Expression
     BinaryExpression,
     PrefixExpression,
     CallExpression,
 
+    // Functions and Accessors
+    Function,
+    Accessor,
+
+    // Literals
+    NumericLiteral,
+    StringLiteral,
+
+    // Contextual keywords
+    BooleanKeyword,
+
     // Punctuation
+    OpenBraceToken,
+    CloseBraceToken,
+    OpenParenToken,
+    CloseParenToken,
+    OpenBracketToken,
+    CloseBracketToken,
+    DotToken,
+    DotDotDotToken,
+    SemicolonToken,
     CommaToken,
+    QuestionDotToken,
+    LessThanToken,
+    LessThanSlashToken,
+    GreaterThanToken,
+    LessThanEqualsToken,
+    GreaterThanEqualsToken,
+    EqualsEqualsToken,
+    EqualsTildeToken,
+    ExclamationEqualsToken,
+    ExclamationTildeToken,
+    EqualsEqualsEqualsToken,
+    ExclamationEqualsEqualsToken,
+    EqualsGreaterThanToken,
+    PlusToken,
+    MinusToken,
+    AsteriskToken,
+    AsteriskAsteriskToken,
+    SlashToken,
+    PercentToken,
+    PlusPlusToken,
+    MinusMinusToken,
+    LessThanLessThanToken,
+    GreaterThanGreaterThanToken,
+    GreaterThanGreaterThanGreaterThanToken,
+    AmpersandToken,
+    BarToken,
+    CaretToken,
+    ExclamationToken,
+    TildeToken,
+    AmpersandAmpersandToken,
+    BarBarToken,
     QuestionToken,
     ColonToken,
+    AtToken,
     QuestionQuestionToken,
-    MinusToken,
-    TildeToken,
-    ExclamationToken
+    InToken
 }
 
 export interface ReadonlyTextRange {
@@ -43,7 +99,7 @@ export type PrefixOperator
 export interface PrefixExpression extends Expression {
     readonly kind: SyntaxKind.PrefixExpression
     readonly operator: PrefixOperator
-    readonly operand: CallExpression | Identifier
+    readonly operand: CallExpression | AccessorExpression
 }
 
 export interface ExpressionArray extends ReadonlyArray<Expression>, ReadonlyTextRange {
@@ -51,10 +107,117 @@ export interface ExpressionArray extends ReadonlyArray<Expression>, ReadonlyText
 
 export interface CallExpression extends Expression {
     readonly kind: SyntaxKind.CallExpression
-    readonly expression: Identifier
+    readonly expression: SyntaxKind.Function
     readonly arguments: ExpressionArray
 }
 
-export interface Identifier extends Expression {
+export interface AccessorExpression extends Expression {
+    readonly kind: SyntaxKind.Accessor;
     readonly escapedText: string
+}
+
+export interface Goevaluate {
+    readonly kind: SyntaxKind.Goevaluate;
+    text: string
+    parseDiagnostics: Diagnostic[]
+}
+
+export enum CharacterCodes {
+    // Unicode's White Space
+    tab = 0x09,                   // \t
+    lineFeed = 0x0A,              // \n
+    verticalTab = 0x0B,           // \v
+    formFeed = 0x0C,              // \f
+    carriageReturn = 0x0D,        // \r
+    space = 0x0020,               // " "
+
+    a = 0x61,
+    b = 0x62,
+    c = 0x63,
+    d = 0x64,
+    e = 0x65,
+    f = 0x66,
+    g = 0x67,
+    h = 0x68,
+    i = 0x69,
+    j = 0x6A,
+    k = 0x6B,
+    l = 0x6C,
+    m = 0x6D,
+    n = 0x6E,
+    o = 0x6F,
+    p = 0x70,
+    q = 0x71,
+    r = 0x72,
+    s = 0x73,
+    t = 0x74,
+    u = 0x75,
+    v = 0x76,
+    w = 0x77,
+    x = 0x78,
+    y = 0x79,
+    z = 0x7A,
+
+    A = 0x41,
+    B = 0x42,
+    C = 0x43,
+    D = 0x44,
+    E = 0x45,
+    F = 0x46,
+    G = 0x47,
+    H = 0x48,
+    I = 0x49,
+    J = 0x4A,
+    K = 0x4B,
+    L = 0x4C,
+    M = 0x4D,
+    N = 0x4E,
+    O = 0x4F,
+    P = 0x50,
+    Q = 0x51,
+    R = 0x52,
+    S = 0x53,
+    T = 0x54,
+    U = 0x55,
+    V = 0x56,
+    W = 0x57,
+    X = 0x58,
+    Y = 0x59,
+    Z = 0x5a,
+
+    _0 = 0x30,
+    _9 = 0x39,
+
+    _ = 0x5F,
+
+    ampersand = 0x26,             // &
+    asterisk = 0x2A,              // *
+    at = 0x40,                    // @
+    backslash = 0x5C,             // \
+    backtick = 0x60,              // `
+    bar = 0x7C,                   // |
+    caret = 0x5E,                 // ^
+    closeBrace = 0x7D,            // }
+    closeBracket = 0x5D,          // ]
+    closeParen = 0x29,            // )
+    colon = 0x3A,                 // :
+    comma = 0x2C,                 // ,
+    dot = 0x2E,                   // .
+    doubleQuote = 0x22,           // "
+    equals = 0x3D,                // =
+    exclamation = 0x21,           // !
+    greaterThan = 0x3E,           // >
+    hash = 0x23,                  // #
+    lessThan = 0x3C,              // <
+    minus = 0x2D,                 // -
+    openBrace = 0x7B,             // {
+    openBracket = 0x5B,           // [
+    openParen = 0x28,             // (
+    percent = 0x25,               // %
+    plus = 0x2B,                  // +
+    question = 0x3F,              // ?
+    semicolon = 0x3B,             // ;
+    singleQuote = 0x27,           // '
+    slash = 0x2F,                 // /
+    tilde = 0x7E,                 // ~
 }
